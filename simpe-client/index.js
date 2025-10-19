d2ba0bb0404e6673f15ed25124ece01a7198357a
skybrera <74536332+devsterit@users.noreply.github.com>
2025-10-19 23:08:11 +0200

    add functionalities to index.js + add global style + add component in index.html

--- simpe-client/index.js
+++ simpe-client/index.js
@@ -1,1 +1,17 @@
-alert('questa è una prova!');
+document.addEventListener('DOMContentLoaded', function() {
+
+    // alert('questa è una prova!');
+
+    function insertInfoInDOM() {
+        let button = document.getElementById('generate');
+        let data = document.getElementById('data');
+
+
+        button.addEventListener('click', function() {
+            data.innerHTML = 'QUESTA E\' UNA PROVA';
+        });
+    }
+
+
+    insertInfoInDOM();
+});
